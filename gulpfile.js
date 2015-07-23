51d5fc96db06f74c44f81fee36c1d400d953ba0f
Ethan Larkham <ethanlarkham@gmail.com>
2015-07-23 11:10:21 -0400

    Renamed app.js to server.js, added gulp task that scans my javascript for errors.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp   = require('gulp');
 var sass   = require('gulp-sass');
+var jshint = require('gulp-jshint');
 
 gulp.task('css', function(){
-        gulp.src('./sass/**/*.scss')
+        return gulp.src('./sass/**/*.scss')
                 .pipe(sass().on('error', sass.logError))
                 .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function() {
         gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('js', function(){
+
+        return gulp.src(['server.js', 'public/app/*.js', 'public/app/**/*.js', 'app/**/*.js'])
+                .pipe(jshint())
+                .pipe(jshint.reporter('default'));
 });
 

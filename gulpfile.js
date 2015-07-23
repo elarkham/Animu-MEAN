a3a082a6cc6d47da0514283315c81a1fbbc2b2f5
Ethan Larkham <ethanlarkham@gmail.com>
2015-07-23 11:00:17 -0400

    Added gulp and bower to handle my scss files and allow me to store local copys of angular and the like.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+
+gulp.task('css', function(){
+        gulp.src('./sass/**/*.scss')
+                .pipe(sass().on('error', sass.logError))
+                .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+        gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
